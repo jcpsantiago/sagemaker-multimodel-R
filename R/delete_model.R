ec1077b3b684f58fa91256c3553e8a847ddcbbd6
João Santiago <joaocpsantiago@gmail.com>
2020-07-26 18:34:02 +0200

    update delete_model function

--- R/delete_model.R
+++ R/delete_model.R
@@ -1,18 +1,19 @@
 #' Unload loaded model
-#'
+#' 
+#' @param model_name Name of the model
 #' @param req Request object
 #' @param res Response object
 #'
 #' @return Unloads a loaded model
 #' @export
-delete_model <- function(req, res) {
+delete_model <- function(model_name, req, res) {
   if (!model_name %in% loaded_models$modelName) {
     message("Model `", model_name, "` is not loaded!")
     res$status <- 404
     return("")
   }
 
-  message("Deleting model `", model_name, "`")
+  message("Unloading model `", model_name, "`")
   loaded_models <<- loaded_models[!loaded_models$modelName == model_name, , drop = FALSE]
   rm(list = eval(model_name), envir = globalenv())
 
